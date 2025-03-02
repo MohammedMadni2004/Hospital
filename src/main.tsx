89013af6634ee756089cb336ac4c32e92eec3c26
Lakshmi S Hiremath <laharish2004@gmail.com>
2025-03-02 13:30:51 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";  // Importing App component
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
+import Appointment from './pages/Appointment.tsx';
+import Telemedicine from './pages/Telemedicine.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/dashboard',
+    element: <App />,
+  },
+  {
+    path: '/appointment',
+    element: <Appointment />,
+  },
+  {
+    path: '/telemedicine',
+    element: <Telemedicine />,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
