30c8a734524e6d44554e23d97d178b650d6248ee
abhisfamed <166108215+abhisfamed@users.noreply.github.com>
2025-03-15 19:42:23 +0530

    added schema for db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('he;llo wd');
+import express from 'express'
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({success: true, message :"heelo world"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
